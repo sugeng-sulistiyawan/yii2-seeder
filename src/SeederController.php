cd93a2acf73fcda16421381bf68166777dc90f64
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2024-04-17 14:28:14 +0700

    fixed bugs auto create seeder

--- src/SeederController.php
+++ src/SeederController.php
@@ -345,7 +345,7 @@ class SeederController extends Controller
     protected function getDefaultSeeder()
     {
         $defaultSeederClass = "{$this->seederNamespace}\\{$this->defaultSeederClass}";
-        $defaultSeederFile = "{$defaultSeederClass}.php";
+        $defaultSeederFile  = str_replace('\\', DIRECTORY_SEPARATOR, "{$defaultSeederClass}.php");
 
         if (!class_exists($defaultSeederClass) || !file_exists($defaultSeederFile)) {
             FileHelper::createDirectory($this->seederPath);
